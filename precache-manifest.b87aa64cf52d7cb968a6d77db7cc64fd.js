d2163a2e7ae20007fff4c00e84e4bc6aa57570ce
Ruben Rivera <rerivera76@gmail.com>
2021-03-26 13:40:14 -0400

    Updates

---
+++ precache-manifest.b87aa64cf52d7cb968a6d77db7cc64fd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ddfc89efb19cd06a337b815366a416f3",
+    "url": "/placess-app/index.html"
+  },
+  {
+    "revision": "cfc413c2ded1933fba57",
+    "url": "/placess-app/static/css/main.80509910.chunk.css"
+  },
+  {
+    "revision": "6ce3e522594356c56d10",
+    "url": "/placess-app/static/js/2.3eb0a724.chunk.js"
+  },
+  {
+    "revision": "cfc413c2ded1933fba57",
+    "url": "/placess-app/static/js/main.9b7f81ca.chunk.js"
+  },
+  {
+    "revision": "b4c239864c20e96bd884",
+    "url": "/placess-app/static/js/runtime-main.665c3c6b.js"
+  }
+]);
